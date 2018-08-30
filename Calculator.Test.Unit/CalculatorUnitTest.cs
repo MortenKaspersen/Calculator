262b9d33b48fc9f09ca5842082c625a1f0060308
MortenKaspersen <38438662+MortenKaspersen@users.noreply.github.com>
2018-08-30 11:08:57 +0200

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        [Test]
+        public void Add_Add2And4_Returns6()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Add(2, 4), Is.EqualTo(6));
+        }
+    }
+}
